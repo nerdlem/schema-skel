78db0ef32aec1fcfb1d4548b7a4c8d1cb7a4694c
Luis Muñoz <lem@lem.click>
2023-03-12 12:11:38 -0400

    Doc purpose

--- db/schema-deltas/000000-example-delta.sql
+++ db/schema-deltas/000000-example-delta.sql
@@ -2,6 +2,9 @@ BEGIN;
 
 SET search_path TO :"nspace", public;
 
+-- This is the basis for a schema delta. Feel free to remove this delta entirely
+-- or reuse as the basis for a real delta.
+
 INSERT INTO :"nspace".schema_errata ( delta )
 VALUES ( '000000-example-delta' );
 
